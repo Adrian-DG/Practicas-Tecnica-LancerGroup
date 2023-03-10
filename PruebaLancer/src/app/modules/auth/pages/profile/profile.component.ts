556b236a1594014b82e3b97c542de91b3389c310
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2023-03-09 22:49:41 -0400

    chore(media): :bento: add new assets (images) to be used

--- PruebaLancer/src/app/modules/auth/pages/profile/profile.component.ts
+++ PruebaLancer/src/app/modules/auth/pages/profile/profile.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss'],
+	selector: 'app-profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent  implements OnInit {
+export class ProfileComponent implements OnInit {
+	imageUrl: string = '';
 
-  constructor() { }
+	didImageChange: boolean = false;
 
-  ngOnInit() {}
+	constructor() {}
 
+	ngOnInit() {}
+
+	private saveToBase64(imageBase64: string | undefined): void {
+		if (imageBase64) {
+			localStorage.removeItem('userProfilePic');
+			localStorage.setItem('userProfilePic', imageBase64);
+		}
+	}
+
+	async getPicture() {
+		const image = await Camera.getPhoto({
+			quality: 90,
+			allowEditing: true,
+			resultType: CameraResultType.Uri,
+		});
+
+		this.saveToBase64(image.base64String);
+		this.imageUrl = image.webPath ?? '';
+		this.didImageChange = true;
+	}
 }
